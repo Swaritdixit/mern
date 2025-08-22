c1be682fab9da8b6085737780da86ea0d060d731
Swarit Dixit <swaritdixit351@gmail.com>
2025-08-23 00:07:42 +0530

    react skeleton done

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -6,8 +6,14 @@ import NoteDetailPage from "./pages/NoteDetailPage";
 import { toast } from "react-hot-toast";
 const App = () => {
   return (
-    <div>
-      <button onClick={()=>toast.success("congrats")}>Click me!</button>
+    <div data-theme="forest">
+      <button className="btn">Button</button>
+<button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-ghost">Ghost</button>
+<button className="btn btn-link">Link</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
