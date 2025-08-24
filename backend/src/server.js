63e6b225e3d76ef77bbde11c80102dd264b9dd47
Swarit Dixit <swaritdixit351@gmail.com>
2025-08-25 01:55:40 +0530

    done

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,15 +4,16 @@ import {connectDB} from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
-
+import path from "path";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
-
-app.use(
+const _dirname=path.resolve()
+if(process.env.NODE_ENV!=="production")
+{app.use(
   cors({
     origin: "http://localhost:5173",
-  }));
+  }));}
 
 app.use(express.json());
 app.use(rateLimiter);
@@ -23,6 +24,12 @@ app.use((req,res,next)=>{
 })
 app.use("/api/notes",notesRoutes);
 
+
+app.use(express.static(path.join(_dirname,"../frontend/dist")))
+if(process.env.NODE_ENV==="production"){
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(_dirname,"../frontend/dist/index.html"))
+});}
 connectDB().then(() => {
 app.listen(PORT, () => {
   console.log("server started on PORT:", PORT);
