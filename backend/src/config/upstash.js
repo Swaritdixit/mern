a3a1db3acbc029a1f38e8ffa805ec11be5cc913e
Swarit Dixit <swaritdixit351@gmail.com>
2025-08-25 14:20:53 +0530

    upstsh

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -2,6 +2,11 @@ import {Ratelimit} from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 import dotenv from "dotenv";
 dotenv.config();
+const redis = new Redis({
+  url: 'https://full-octopus-9040.upstash.io',
+  token: 'ASNQAAImcDEzNjExMjEzYTY1Yjc0ZmNiYTliNDQ1MzJkMmY5MWMyMHAxOTA0MA',
+})
+
 const ratelimit =new Ratelimit({
     redis: Redis.fromEnv(),
     limiter: Ratelimit.slidingWindow(100, "20 s"),
