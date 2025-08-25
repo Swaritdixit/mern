c3256877002c4562ea06018f0918d000ffa6dd4a
Swarit Dixit <swaritdixit351@gmail.com>
2025-08-25 14:33:34 +0530

    Test

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,9 +1,9 @@
 import express from"express";
-import{getAllNotes,getNoteById,createNote,updateNote,deleteNote} from "../controllers/nodesController.js"
+import{getAllNotes,createNote,updateNote,deleteNote} from "../controllers/nodesController.js"
 const router =express.Router();
 
 router.get("/",getAllNotes);
-router.get("/:id",getAllNotes);
+
 
 router.post("/",createNote);
 router.put("/:id",updateNote);
