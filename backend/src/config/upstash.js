cc8fab08842a820e84c381fc7e63cb51348167c6
Swarit Dixit <swaritdixit351@gmail.com>
2025-08-25 14:30:06 +0530

    sff

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -2,10 +2,6 @@ import {Ratelimit} from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 import dotenv from "dotenv";
 dotenv.config();
-const redis = new Redis({
-  url: UPSTASH_REDIS_REST_URL,
-  token: UPSTASH_REDIS_REST_TOKEN
-})
 
 const ratelimit =new Ratelimit({
     redis: Redis.fromEnv(),
